322b915725cb7e1fafc6b547d5e0aa122a6089f2
Hayelom Tizazu <hayelomtizazu19@gmail.com>
2021-11-11 15:47:51 +0300

    test setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import { store } from './app/store';
 import { Provider } from 'react-redux';
+// import { ToastContainer } from 'react-toastify';
+// import App from './App';
+import { store } from './app/store';
 import 'react-toastify/dist/ReactToastify.css';
-import { ToastContainer } from 'react-toastify';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
-      <ToastContainer />
+      <div className="bg-red-300 h-screen">Homer Index</div>
+      {/* <App />
+      <ToastContainer /> */}
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
